8c70e89cd7fed6892d7bd104bb056350e5c30877
Alif Nugroho <101256067+Ryoto28@users.noreply.github.com>
2022-07-16 09:56:10 +0700

    Add files via upload

---
+++ ProjectMahasiswa/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    class Program
+    {
+        Mahasiswa Mahasiswa = new Mahasiswa();
+        static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>();
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi UAS Matakuliah Pemrograman";
+
+            while (true)
+            {
+                TampilMenu();
+
+                Console.Write("\nNomor Menu [1..3]: ");
+                int nomorMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (nomorMenu)
+                {
+                    case 1:
+                        TambahMahasiswa();
+                        break;
+
+                    case 2:
+                        TampilMahasiswa();
+                        break;
+
+                    case 3: 
+                        return;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void TampilMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Pilih Menu Aplikasi\n");
+            Console.WriteLine("1. Tambah Mahasiswa");
+            Console.WriteLine("2. Tampilkan Mahasiswa");
+            Console.WriteLine("3. Keluar");
+        }
+
+        static void TambahMahasiswa()
+        {
+
+            Console.Clear();
+
+            Mahasiswa Mahasiswa = new Mahasiswa();
+
+            Console.Write("NIM: ");
+            Mahasiswa.Nim = Console.ReadLine();
+            Console.Write("Nama: ");
+            Mahasiswa.Nama = Console.ReadLine();
+            Console.Write("Jenis Kelamin [L/P] : ");
+            Mahasiswa.Gender = Console.ReadLine();
+            if (Mahasiswa.Gender == "L")
+            {
+                Mahasiswa.Gender = "Laki-Laki";
+            }
+            else if (Mahasiswa.Gender == "P")
+            {
+                Mahasiswa.Gender = "Perempuan";
+            }
+            Console.Write("IPK : ");
+            Mahasiswa.Ipk = Console.ReadLine();
+
+            daftarMahasiswa.Add(Mahasiswa);
+
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void TampilMahasiswa()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Data Mahasiswa\n");
+            int no = 1;
+
+            Console.WriteLine("No\tNIM\tNama \tJenis Kelamin\tIPK");
+            foreach (Mahasiswa mahasiswa in daftarMahasiswa)
+            {
+
+                Console.WriteLine("{0}.\t{1}\t{2} \t{3}\t{4}", no, mahasiswa.Nim, mahasiswa.Nama, mahasiswa.Gender, mahasiswa.Ipk);
+                no++;
+            }
+
+            Console.WriteLine("\nTekan enter untuk kembali ke menu");
+            Console.ReadKey();
+        }
+    }
+}
